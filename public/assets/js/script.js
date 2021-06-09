633351efc12de04f360ad29c0e16db50428ffbe4
Sebastian Sierra <ysebasst@gmail.com>
2021-06-09 16:32:24 -0500

    challenge completed

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,3 @@
-const $size = document.querySelector(".size");
-$size.style =
-  "z-index:100;position: absolute; top: 1rem; left: 1rem; font-size: 1.5rem; font-weight: bold";
-
-setInterval(() => {
-  $size.textContent = window.visualViewport.width;
-}, 1000);
-
 const btnToggleMenu = document.getElementById("btn-toggle-menu");
 const navMenu = document.getElementById("nav-menu");
 
